919bf40e4e667df2c93cfc7bea33ea727c33b67c
Memory Clutter <memclutter@gmail.com>
2015-08-11 14:10:16 +0300

    Ack consume

--- components/Consumer.php
+++ components/Consumer.php
@@ -2,16 +2,30 @@
 
 namespace starcode\amqp\components;
 
+use yii\helpers\ArrayHelper;
+
 /**
  * AMQP consumer component.
  */
 class Consumer extends BaseComponent
 {
-    public function consume($callback, $routingKey = '')
+    public function consume($callback, $routingKey = '', $consumeOptions = [])
     {
         list($queueName, ,) = $this->channel->queue_declare("", false, false, true, false);
         $this->channel->queue_bind($queueName, $this->exchange, $routingKey);
-        $this->channel->basic_consume($queueName, '', false, false, false, false, $callback);
+        $options = ArrayHelper::merge([
+            'consumerTag' => '',
+            'noLocal' => false,
+            'noAck' => false,
+            'exclusive' => false,
+            'noWait' => false,
+        ], $consumeOptions);
+        $this->channel->basic_consume($queueName,
+            $options['consumerTag'],
+            $options['noLocal'],
+            $options['noAck'],
+            $options['exclusive'],
+            $options['noWait'], $callback);
     }
 
     public function wait()
