e582e2931d31d478976dca44a5ae26351af5219c
Memory Clutter <memclutter@gmail.com>
2015-08-11 13:47:08 +0300

    Update AmqpProducerTrait

--- AmqpProducerTrait.php
+++ AmqpProducerTrait.php
@@ -7,17 +7,17 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 trait AmqpProducerTrait
 {
-    public function amqpPublishText($body, $routingKey = '', $producerOptions = [])
+    public function amqpPublishText($body, $routingKey = '', $messageProperties = null, $producerOptions = [])
     {
         $message = new AMQPMessage($body, ['content_type' => 'text/plain']);
         $producer = new Producer($producerOptions);
-        $producer->publish($message, $routingKey);
+        $producer->publish($message, $routingKey, $messageProperties);
     }
 
-    public function amqpPublishJson(array $body, $routingKey = '', $producerOptions = [])
+    public function amqpPublishJson(array $body, $routingKey = '', $messageProperties = null, $producerOptions = [])
     {
         $message = new AMQPMessage($body, ['content_type' => 'application/json']);
         $producer = new Producer($producerOptions);
-        $producer->publish($message, $routingKey);
+        $producer->publish($message, $routingKey, $messageProperties);
     }
 }
