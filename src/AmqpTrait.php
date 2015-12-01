b13810dd29d986781809ce950ff95bd5716877f6
Memory Clutter <memclutter@gmail.com>
2015-12-01 17:30:51 +0300

    Adding helper trait

---
+++ src/AmqpTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace starcode\amqp;
+
+use starcode\amqp\components\Connection;
+use Yii;
+
+class AmqpTrait
+{
+    public $amqpConnectionComponentId = 'amqp';
+
+    /**
+     * @param $id
+     * @return Queue
+     */
+    public function amqpGetQueue($id)
+    {
+        return $this->amqpGetConnection()
+            ->getQueue($id);
+    }
+
+    /**
+     * @return null|Connection
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function amqpGetConnection()
+    {
+        return Yii::$app->get($this->amqpConnectionComponentId);
+    }
+}
