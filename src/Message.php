8276ce54bddf1c754f93855ae50ec9541cc47b87
Alexey Kolmakov <alexey.kolmakov@starcode.ru>
2015-12-04 10:51:21 +0300

    Enhance Message class

--- src/Message.php
+++ src/Message.php
@@ -6,5 +6,12 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 class Message extends AMQPMessage
 {
-
+    /**
+     * Set persist mode (delivery_mode = 2)
+     * @return $this
+     */
+    public function persist(){
+        $this->set('delivery_mode', 2);
+        return $this;
+    }
 }
